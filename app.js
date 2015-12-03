5ba919c5cf67124fa86d3f4b914b79a41ebe4c7f
gi-os <gio@gi-os.com>
2015-12-03 15:44:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-<script src="particles.js" type='text/javascript'></script>
+ src="particles.js" type='text/javascript'
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
